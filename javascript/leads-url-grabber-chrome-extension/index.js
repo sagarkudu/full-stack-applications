0c765e72aec89277825693af448f2af7cace7ce7
TC-ITap <sagarkudu@triplecommedia.com>
2026-06-15 17:15:25 +0530

    convert web project to chrome extension

--- javascript/leads-url-grabber-chrome-extension/index.js
+++ javascript/leads-url-grabber-chrome-extension/index.js
@@ -1,10 +1,13 @@
 let myLeads = [];
 const inputEl = document.getElementById("input-el");
 const inputBtn = document.getElementById("input-btn");
+const tabBtn = document.getElementById("tab-btn");
 const deleteBtn = document.getElementById("delete-btn");
 const ulEl = document.getElementById("ul-el");
 
 let leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
+
+const tabs = [{ url: "https://www.linkedin.com/in/sagarkudu/" }];
 
 //checking localstorage before rendering
 if (leadsFromLocalStorage) {
@@ -25,6 +28,15 @@ inputBtn.addEventListener("click", function () {
   inputEl.value = "";
   localStorage.setItem("myLeads", JSON.stringify(myLeads));
   render(myLeads);
+});
+
+//get current tab and its url
+tabBtn.addEventListener("click", function () {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    myLeads.push(tabs[0].url);
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+  });
 });
 
 function render(leads) {
